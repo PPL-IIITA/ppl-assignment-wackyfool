ea4a5c6f43704a7f806afd3f7f44dc51dc98598d
wackyfool <agarwal.pranjal96@gmail.com>
2017-02-25 19:08:59 +0530

    Hum se na ho payega

---
+++ boys.cpp
@@ -0,0 +1,15 @@
+class girls{
+	string name;
+	int attractiveness;
+	double budget;
+	int intelligence;
+	public:
+		girls(string n,int a,double b,int i)
+		{
+			name=n;
+			attractiveness=a;
+			budget=b;
+			intelligence=i;
+		}
+		
+};
